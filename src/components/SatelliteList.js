c12c9392b0705bbd37074e1d7e0316227520f34d
Zelin Zhao <zzhao36@cs.uchicago.edu>
2020-08-23 19:46:42 -0500

    draw a world map under main component by d3

--- src/components/SatelliteList.js
+++ src/components/SatelliteList.js
@@ -3,12 +3,20 @@ import { List, Avatar, Button, Checkbox, Spin } from 'antd';
 import Satellite from "../assets/images/Satellite.svg";
 
 class SatelliteList extends Component {
+    onChange = e => {
+        const { dataInfo, checked } = e.target;
+        this.props.onSelectionChange(dataInfo, checked);
+    }
+
     render() {
         const satList = this.props.satInfo ? this.props.satInfo.above : [];
         return (         
             <div className="sat-list-box">
                 <Button className="sat-list-btn"
-                        size="large">Track on the map</Button>
+                        size="large"
+                        disabled={this.props.disableTrack}
+                        onClick={() => this.props.trackOnclick()}
+                >Track on the map</Button>
                 <hr/>
                 {
                     this.props.loading ? 
